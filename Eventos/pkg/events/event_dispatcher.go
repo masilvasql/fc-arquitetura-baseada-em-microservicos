539b215fa9c77f8cbff8743f43abcbcf4c3d8d6b
marcelo.silva <marcelo.silva@neomind.com.br>
2023-07-06 21:19:28 -0300

    iniciado com eventos

---
+++ Eventos/pkg/events/event_dispatcher.go
@@ -0,0 +1,30 @@
+package events
+
+import "errors"
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+var ErrHandlerAlreadyRegistered = errors.New("Handler already registered")
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+
+	return nil
+}
---
+++ Eventos/pkg/events/interfaces.go
@@ -0,0 +1,24 @@
+package events
+
+import "time"
+
+type EventInterface interface {
+	GetName() string
+	GetDateTime() time.Time
+	GetPayload() interface{}
+}
+
+type EventHandlerInterface interface {
+	Handle(event EventInterface)
+}
+
+type EventDispatcherInterface interface {
+	Register(eventName string, handler EventHandlerInterface) error
+	Dispatch(event EventInterface) error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear() error
+}
+
+//Evento - evento criado
+//Dispatcher - responsavel por disparar o evento
